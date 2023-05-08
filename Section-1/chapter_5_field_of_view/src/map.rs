c244fdefbd60d542c5ed487e9907b426ff4be189
jiesamb <samb233@hotmail.com>
2023-05-08 21:12:09 +0800

    chapter5: step1: refector map

---
+++ Section-1/chapter_5_field_of_view/src/map.rs
@@ -0,0 +1,135 @@
+use std::cmp::{max, min};
+
+use rltk::{Rltk, RGB};
+
+use crate::Rect;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+    pub rooms: Vec<Rect>,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl Map {
+    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y * self.width) as usize + x as usize
+    }
+
+    fn apply_room_to_map(&mut self, room: &Rect) {
+        for x in room.x1 + 1..room.x2 {
+            for y in room.y1 + 1..room.y2 {
+                let idx = self.xy_idx(x, y);
+                self.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        for x in min(x1, x2)..max(x1, x2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < (self.width * self.height) as usize {
+                self.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        for y in min(y1, y2)..max(y1, y2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < (self.width * self.height) as usize {
+                self.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
+    pub fn new_map_rooms_and_corridors() -> Map {
+        let mut map = Map {
+            tiles: vec![TileType::Wall; 80 * 50],
+            rooms: Vec::new(),
+            width: 80,
+            height: 50,
+        };
+
+        const MAX_ROOMS: i32 = 30;
+        const MIN_SIZE: i32 = 6;
+        const MAX_SIZE: i32 = 10;
+
+        let mut rng = rltk::RandomNumberGenerator::new();
+
+        for _ in 0..MAX_ROOMS {
+            let w = rng.range(MIN_SIZE, MAX_SIZE);
+            let h = rng.range(MIN_SIZE, MAX_SIZE);
+            let x = rng.roll_dice(1, 80 - w - 1) - 1;
+            let y = rng.roll_dice(1, 50 - h - 1) - 1;
+
+            let room = Rect::new(x, y, w, h);
+            let mut ok: bool = true;
+            for other in map.rooms.iter() {
+                if room.intersect(other) {
+                    ok = false;
+                }
+            }
+
+            if ok {
+                map.apply_room_to_map(&room);
+
+                if !map.rooms.is_empty() {
+                    let (new_x, new_y) = room.center();
+                    let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].center();
+                    if rng.range(0, 2) == 1 {
+                        map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, new_x);
+                    } else {
+                        map.apply_horizontal_tunnel(prev_x, new_x, new_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, prev_y);
+                    }
+                }
+
+                map.rooms.push(room)
+            }
+        }
+
+        map
+    }
+}
+
+pub fn draw_map(map: &Map, ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.tiles.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            y += 1;
+            x = 0;
+        }
+    }
+}
---
+++ Section-1/chapter_5_field_of_view/src/player.rs
@@ -0,0 +1,41 @@
+use rltk::{Rltk, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+
+use crate::{Map, Player, Position, State, TileType};
+
+pub fn move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut position = ecs.write_storage::<Position>();
+    let mut player = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Map>();
+
+    for (_player, pos) in (&mut player, &mut position).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(39, max(0, pos.y + delta_y));
+        }
+    }
+}
+
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
+                move_player(-1, 0, &mut gs.ecs)
+            }
+
+            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
+                move_player(1, 0, &mut gs.ecs)
+            }
+            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
+                move_player(0, -1, &mut gs.ecs)
+            }
+            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
+                move_player(0, 1, &mut gs.ecs)
+            }
+            _ => {}
+        },
+    }
+}
